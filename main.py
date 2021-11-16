96345be13ca9522ee04557506e50eda5af928ff4
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-11-16 13:35:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 base = int(input("What's the base of the triangle? "))
 height = int(input("What's the height of the triangle? "))
 print("The area of the right angle triangle is ", base*height/2)
+#gives area of right angle triangle
